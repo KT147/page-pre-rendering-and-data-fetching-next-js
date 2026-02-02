577a015a53ba6c79d4f30abe9455ff121938ed75
KT147 <kevin.tammm@gmail.com>
2026-02-02 14:30:57 +0200

    useSWR()

---
+++ pages/last-sales.js
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+function LastSalesPage() {
+  const [sales, setSales] = useState([]);
+  //   const [isloading, setIsLoading] = useState(false);
+
+  const { data, error } = useSWR(
+    "https://database-982fe-default-rtdb.europe-west1.firebasedatabase.app/sales.json",
+    (url) => fetch(url).then((res) => res.json())
+  );
+
+  useEffect(() => {
+    if (data) {
+      const transformedSales = [];
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+
+      setSales(transformedSales);
+    }
+  }, [data]);
+
+  //   useEffect(() => {
+  //     setIsLoading(true);
+  //     fetch(
+  //       "https://database-982fe-default-rtdb.europe-west1.firebasedatabase.app/sales.json"
+  //     )
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         const transformedSales = [];
+  //         for (const key in data) {
+  //           transformedSales.push({
+  //             id: key,
+  //             username: data[key].username,
+  //             volume: data[key].volume,
+  //           });
+  //         }
+  //         setSales(transformedSales);
+  //         setIsLoading(false);
+  //       });
+  //   }, []);
+
+  if (error) {
+    return <p>Failed to load.</p>;
+  }
+
+  if (!data && !sales) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <ul>
+      {sales.map((sale) => (
+        <li key={sale.id}>
+          {sale.username} - ${sale.volume}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default LastSalesPage;
